89a2209736d06dc838faa4fb05f595d2d9f8dcc5
xaviduds <xaviduds@gmail.com>
2024-04-28 18:40:49 -0300

    Testing initial order

--- scripts/projects.js
+++ scripts/projects.js
@@ -12,34 +12,38 @@ export const projects = [
     summary: 'Tool for registry and connection between Needs and Contributions with open scope.',
     modifiedDate: '2023-04-23',
     showcasePath: 'src/lince/showcase.html',
-    startingOrder: 1,
+    startingOrder: 2,
   },
   {
     name: 'NixOS',
     tags: ['Linux', 'Nix'],
     summary: 'The workings of my Linux distribution + my configuration.',
     modifiedDate: '2023-04-28',
-    showcasePath: 'src/nixos/showcase.html'
+    showcasePath: 'src/nixos/showcase.html',
+    startingOrder: 1,
   },
   {
     name: 'Purchased',
     tags: ['ML', 'Python'],
     summary: 'Prediction of purchasing.',
     modifiedDate: '2023-04-23',
-    showcasePath: 'src/purchased/showcase.html'
+    showcasePath: 'src/purchased/showcase.html',
+    startingOrder: 3,
   },
   {
     name: 'R.U.S.T',
     tags: ['Rust'],
     summary: '(R)andom R(u)st (S)cripts for (T)rainning.',
     modifiedDate: '2023-04-23',
-    showcasePath: 'src/rust_learning/showcase.html'
+    showcasePath: 'src/rust_learning/showcase.html',
+    startingOrder: 0,
   },
   // {
   //   name: 'Titanic',
   //   tags: ['ML', 'Python'],
   //   summary: 'The famous titanic dataset.',
   //   modifiedDate: '2023-04-23',
-  //   showcasePath: 'src/titanic/showcase.html'
+  //   showcasePath: 'src/titanic/showcase.html',
+  // startingOrder: 1,
   // },
 ];
