88c057816b42daaf7395d0a95fc1576eaf995ea3
xaviduds <xaviduds@gmail.com>
2024-05-02 21:38:12 -0300

    Naming overhaul

---
+++ scripts/projects.js
@@ -0,0 +1,39 @@
+export const projects = [
+  // {
+  //   name: '',
+  //   summary: '',
+  //   tags: [''],
+  //   showcasePath: ''
+  // },
+  {
+    name: 'Lince',
+    summary: 'Tool for registry and connection between Needs and Contributions with open scope.',
+    tags: [''],
+    showcasePath: 'src/lince/showcase.html',
+  },
+  {
+    name: 'Y.A.B.R.U.S.T',
+    summary: '(Y)et (A)nother (B)unch of (R)andom R(u)st (S)cripts for (T)rainning.',
+    tags: [''],
+    showcasePath: 'src/rust_learning/showcase.html',
+  },
+  // {
+  //   name: 'Purchased',
+  //   summary: 'Prediction of purchasing.',
+  // tags: [''],
+  //   showcasePath: 'src/purchased/showcase.html',
+  // },
+  // {
+  //   name: 'Titanic',
+  //   summary: 'The famous titanic dataset.',
+  // tags: [''],
+  //   showcasePath: 'src/titanic/showcase.html',
+  // },
+  {
+    name: 'NixOS',
+    summary: 'The workings of my Linux distribution + my configuration.',
+    tags: [''],
+    showcasePath: 'src/nixos/showcase.html',
+  },
+];
+
