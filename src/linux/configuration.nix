a22c1ad191d5950820ccf896eca8505de758b35b
xaviduds <xaviduds@gmail.com>
2024-07-10 13:11:07 -0300

    commit

--- src/linux/configuration.nix
+++ src/linux/configuration.nix
@@ -3,6 +3,37 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
+  nix = {
+    optimise = {
+      automatic = true;
+      dates = [ "22:11" ];
+    };
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 1d";
+    };
+  };
+
+  system = {
+    autoUpgrade = {
+      enable = true;
+      flake = inputs.self.outPath;
+      flags = [
+        "--update-input"
+        "nixpkgs"
+        "-L" # print build logs
+      ];
+      dates = "22:11";
+      randomizedDelaySec = "45min";
+    };
+    stateVersion = "24.05";
+  };
+
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
     loader = {
@@ -35,6 +66,12 @@
     '';
   };
 
+  fileSystems."/persist".neededForBoot = true;
+  environment.persistence."/persist/system" = {
+    hideMounts = true;
+    directories = [ "/etc/NetworkManager/system-connections" ];
+  };
+
   users.users.eduardo = {
     isNormalUser = true;
     description = "eduardo";
@@ -42,11 +79,10 @@
     extraGroups = [ "audio" "networkmanager" "wheel" ];
   };
 
-  fileSystems."/persist".neededForBoot = true;
-  environment.persistence."/persist/system" = {
-    hideMounts = true;
-    directories = [ "/etc/NetworkManager/system-connections" ];
-  };
+  systemd.tmpfiles.rules = [
+    "d /persist/home/ 0777 root root -" # create /persist/home owned by root
+    "d /persist/home/eduardo 0700 eduardo users -" # /persist/home/<user> owned by that user
+  ];
 
   home-manager = {
     extraSpecialArgs = { inherit inputs; };
@@ -57,15 +93,6 @@
     fuse.userAllowOther = true;
     hyprland.enable = true;
     dconf.enable = true;
-    # git = {
-    #   enable = true;
-    #   config = {
-    #     user = {
-    #       name = "xaviduds";
-    #       email = "xaviduds@gmail.com";
-    #     };
-    #   };
-    # };
   };
 
   networking = {
@@ -107,126 +134,31 @@
       rustfmt
       vscode-langservers-extracted
     ];
-    # shellAliases = {
-    #   # GIT
-    #   "sshgithub" =
-    #     "ssh-keygen -t ed25519 -C 'xaviduds@gmail.com' && eval '$(ssh-agent -s)' && ssh-add ~/.ssh/id_ed25519 && cat ~/.ssh/id_ed25519.pub";
-
-    #   "s" = "if [ -d .git ]; then git status; fi";
-
-    #   "gd" = "git diff";
-    #   "gl" = "git log -p";
-
-    #   "a" = "git add";
-    #   "aa" = "git add .";
-    #   "ap" = "git add -p";
-    #   "ai" = "git add -i";
-
-    #   "c" = "git commit";
-    #   "m" = "git commit -m";
-
-    #   "gp" = "git pull";
-    #   "gpr" = "git pull --rebase";
-
-    #   "p" = "git push";
-
-    #   "cc" = "aa && m 'commit' && p && zl";
-
-    #   # MOVING AROUND, FINDING OUT
-    #   "z" =
-    #     "clear && bash ~/.karma/scripts/fetch.sh && ls --group-directories-first -AFw1 && echo && s";
-    #   "t" =
-    #     "clear && bash ~/.karma/scripts/fetch.sh && ls --group-directories-first -AFRw1 && echo && s";
-
-    #   "k" = "cd ~/.karma && z";
-    #   "ks" = "cd ~/.karma/scripts && z";
-    #   "v" = "cd ~/.charya && z";
-    #   "n" = "cd ~/.karma/src/linux && z && hx .";
-    #   "d" = "cd ~/Downloads && z";
-    #   "l" = "cd ~/lince && z";
-    #   "co" = "cd ~/.config && z";
-    #   "dc" = "cd ~/ && z";
-    #   "ksrc" = "cd ~/.karma/src && z";
-    #   "lsrc" = "cd ~/lince/src && z";
-
-    #   ".." = "cd ..";
-    #   "e" = "exit";
-
-    #   # NIX
-    #   "ns" = "nix-shell";
-    #   "np" = "nix-shell -p";
-    #   "ff" = "nix-shell -p firefox";
-
-    # "nslince" = "nix-shell ~/lince/default.nix";
-    # "nsl" = "nix-shell ~/lince/default.nix";
-
-    # "nspython" = "nix-shell ~/.karma/samadhi/python.nix";
-    # "nsp" = "nix-shell ~/.karma/samadhi/python.nix";
-
-    # "nsreact" = "nix-shell ~/.karma/samadhi/react.nix";
-
-    # "nsrust" = "nix-shell ~/.karma/samadhi/rust.nix";
-
-    # "nsds" = "nix-shell ~/.karma/samadhi/data_science.nix";
-
-    # "nd" = "nix flake update && nix develop && z";
-    # "u" = "sudo nix flake update ~/.karma/src/linux/";
-    # "b" =
-    #   "sudo nixos-rebuild switch --flake ~/.karma/src/linux#default --impure";
-    # "br" = "b && reboot";
-    # "ncs" = "nix-collect-garbage -d && df -h";
-    # "bah" = "u && b && ncs";
-
-    # # STUDY
-    # "ht" = "hx ~/.charya";
-    # "zt" = "zathura ~/.charya/estudando/* &";
-
-    # # TMUX
-    # "22" =
-    #   "tmux split-window -h \\; split-window -v \\; select-pane -L \\; split-window -v \\; select-pane -U && clear";
-    # "12" =
-    #   "tmux split-window -v \\; split-window -h \\; select-pane -L \\; select-pane -U && clear";
-
-    # # DEV
-    # "f" = "firefox &";
-    # "h" = "hx";
-    # "hk" = "hx ~/.karma";
-
-    # "linsql" = "psql -U postgres -d lince";
-
-    # "hl" = "hx ~/lince/src/app/lince.py";
-    # "linceapploop" =
-    #   "while true; do python ~/lince/src/app/lince.py; sleep 1; done";
-    # "lap" = "while true; do python ~/lince/src/app/lince.py; sleep 1; done";
-
-    # "lincetestloop" =
-    #   "while true; do python ~/lince/org/test/test.py; sleep 1; done";
-    # "ltl" = "while true; do python ~/lince/org/test/test.py; sleep 1; done";
-
-    # # SYSTEM CONTROL
-    # "wifi" = "xterm -e nmtui";
-    # "i" = "bash ~/.karma/scripts/fetch.sh";
-    # "logout" = "pkill -u eduardo";
-
-    # "vt" = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
-    # "vu" = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05+";
-    # "vd" = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05-";
-    # "va" = "wpctl set-volume @DEFAULT_AUDIO_SINK@";
-    # "v0" = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0";
-
-    #   "mt" = "wpctl set-mute @DEFAULT_SOURCE@ toggle";
-    #   "mu" = "wpctl set-volume @DEFAULT_SOURCE@ 0.01+";
-    #   "md" = "wpctl set-volume @DEFAULT_SOURCE@ 0.01-";
-    #   "ma" = "wpctl set-volume @DEFAULT_SOURCE@";
-    #   "m0" = "wpctl set-volume @DEFAULT_SOURCE@ 0";
-    #   "m10" = "wpctl set-volume @DEFAULT_SOURCE@ 0.1";
-
-    #   "bu" = "brightnessctl s +5%";
-    #   "bd" = "brightnessctl s 5%-";
-    # };
   };
 
   services = {
+    thermald.enable = true;
+    power-profiles-daemon.enable = false;
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+        };
+        charger = {
+          governor = "powersave";
+          turbo = "auto";
+        };
+      };
+    };
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      wireplumber.enable = true;
+    };
+
     postgresql = {
       enable = true;
       authentication = pkgs.lib.mkOverride 10 ''
@@ -246,65 +178,5 @@
         };
       };
     };
-
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      wireplumber.enable = true;
-    };
-
-    thermald.enable = true;
-
-    power-profiles-daemon.enable = false;
-
-    auto-cpufreq = {
-      enable = true;
-      settings = {
-        battery = {
-          governor = "powersave";
-          turbo = "never";
-        };
-        charger = {
-          governor = "powersave";
-          turbo = "auto";
-        };
-      };
-    };
-  };
-
-  systemd.tmpfiles.rules = [
-    "d /persist/home/ 0777 root root -" # create /persist/home owned by root
-    "d /persist/home/eduardo 0700 eduardo users -" # /persist/home/<user> owned by that user
-  ];
-
-  nix = {
-    optimise = {
-      automatic = true;
-      dates = [ "22:11" ];
-    };
-    settings = {
-      auto-optimise-store = true;
-      experimental-features = [ "nix-command" "flakes" ];
-    };
-    gc = {
-      automatic = true;
-      dates = "daily";
-      options = "--delete-older-than 1d";
-    };
-  };
-
-  system = {
-    autoUpgrade = {
-      enable = true;
-      flake = inputs.self.outPath;
-      flags = [
-        "--update-input"
-        "nixpkgs"
-        "-L" # print build logs
-      ];
-      dates = "22:11";
-      randomizedDelaySec = "45min";
-    };
-    stateVersion = "24.05";
   };
 }
